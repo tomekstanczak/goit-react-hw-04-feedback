5e5ae5b39bf1a9522d2dbcb9933d2ec4055a7ba2
tomekstanczak <tomek.stanczak87@gmail.com>
2024-02-11 07:52:38 +0100

    refactoring aplication, add hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,74 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import NotificationMsg from './NotificationMsg/NotificationMsg';
 import PropTypes from 'prop-types';
 
-class App extends Component {
-  static defaultProps = {
-    step: 1,
-    initialValue: 0,
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+
+  const handleIncrement = category => {
+    if (category === 'good') {
+      setGood(good + 1);
+    }
+    if (category === 'neutral') {
+      setNeutral(neutral + 1);
+    }
+    if (category === 'bad') {
+      setBad(bad + 1);
+    }
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      good: this.props.initialValue,
-      neutral: this.props.initialValue,
-      bad: this.props.initialValue,
-      total: this.props.initialValue,
-      percentage: this.props.initialValue,
-    };
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-  handleIncrement = category => {
-    this.setState(
-      () => ({
-        [category]: this.state[category] + this.props.step,
-      }),
-      () => {
-        this.countTotalFeedback();
-        this.countPositiveFeedbackPercentage();
-      }
-    );
-  };
-  countTotalFeedback = () => {
-    this.setState(({ good, neutral, bad }) => ({
-      total: good + neutral + bad,
-    }));
-  };
-  countPositiveFeedbackPercentage = () => {
-    this.setState(({ good, total }) => ({
-      percentage: total > 0 ? (good / total) * 100 : 0,
-    }));
+  const countPositiveFeedbackPercentage = newTotal => {
+    setPercentage((good / newTotal) * 100);
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleIncrement}
-          />
-        </Section>
-        <Section title="Statistic">
-          <NotificationMsg
-            message="There is no feedback"
-            total={this.state.total}
-          />
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.state.total}
-            percentage={this.state.percentage}
-          />
-        </Section>
-      </div>
-    );
-  }
+  const countTotalFeedback = () => {
+    setTotal(() => {
+      const newTotal = good + neutral + bad;
+      countPositiveFeedbackPercentage(newTotal);
+      return newTotal;
+    });
+  };
+
+  useEffect(() => {
+    countTotalFeedback();
+  }, [good, neutral, bad]);
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleIncrement}
+        />
+      </Section>
+      <Section title="Statistic">
+        <NotificationMsg message="There is no feedback" total={total} />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          percentage={percentage}
+        />
+      </Section>
+    </div>
+  );
 }
 
 App.propTypes = {
